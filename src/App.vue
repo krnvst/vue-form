b3898704b4b2a5da99a41334e61eed1dc6ee789b
kornev <skorns20@gmail.com>
2021-02-02 21:53:22 +0200

    add form

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <all-forms />
+</template>
+
+<script>
+import AllForms from './components/AllForms.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AllForms
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 40px auto;
+  max-width: 500px;
+}
+</style>
